a6d376561a6949972598365f22fe1b6cabfad407
Stephen Key <skey2@uco.edu>
2026-08-24 18:03:07 -0400

    Wrestling with Mystery's Gambit

--- sample_mods/MysterysGambit/src/GambitMystery.cs
+++ sample_mods/MysterysGambit/src/GambitMystery.cs
@@ -1,24 +1,24 @@
 using System;
 using System.Reflection;
-using System.Collections;
-using System.Collections.Generic;
 using System.Linq;
 using Blukulele.CHE;
 using Blukulele.Core;
 using UnityEngine;
-using DG.Tweening;
+using UnityEngine.UI;
 
 namespace Gambonanza.MysterysGambit
 {
     /// <summary>
     /// Mystery's Gambit: Mimics the effects of three random gambits.
     /// 
-    /// 
+    /// WIP: If one of this gambit's sub-gambits normally displays a sprite on top of it (Prayer, Violet, etc)
+    /// It tends to throw game-breaking exceptions. I'm still looking for the cause of this error.
     /// </summary>
     public sealed class GambitMystery : BaseGambit
     {
         // Define the three child gambits for this gambit to mimic.
-        private BaseGambit[] children = new BaseGambit[3];
+        //private BaseGambit[] children = new BaseGambit[3];
+        private GambitBehaviour[] children = new GambitBehaviour[3];
         // Define a list of every child class of BaseGambit and a randomizer to select from it
         private Type[] allGambits = AppDomain.CurrentDomain.GetAssemblies()
                 .SelectMany(assembly => assembly.GetTypes())
@@ -27,12 +27,15 @@ namespace Gambonanza.MysterysGambit
         private System.Random pick = new();
         private void Start()
         {
-            // Populate the children list with three random instances of BaseGambit
-            for (int x = 0; x < children.Length; x++)
-            {
-                children[x] = CreateGambitInstance(allGambits[pick.Next(allGambits.Length)]);
-            }
-            SelectionManager.Instance.OnPawnPromotionAsMoved += ShowcaseMode;//Shhhhh
+                string s = ""; //DEBUG
+                // Populate the children list with three random instances of BaseGambit
+                for (int x = 0; x < children.Length; x++)
+                {
+                    children[x] = CreateGambitInstance(allGambits[pick.Next(allGambits.Length)]);
+                    s += children[x].Info.GambitName + '\n';//DEBUG
+                }
+                PromotionManager.Instance.OnPromotePlayer += ShowcaseMode;//Shhhhh
+                UpdateDescription(s);//DEBUG
         }
 
         private void OnDestroy()
@@ -40,16 +43,17 @@ namespace Gambonanza.MysterysGambit
             // Delete each child gambit
             for (int x = 0; x < children.Length; x++)
             {
-                Destroy(children[x]);
+                Destroy(children[x].gameObject);
                 children[x] = null;
             }
-            SelectionManager.Instance.OnPawnPromotionAsMoved -= ShowcaseMode;//Nothing to see here ;)
+            PromotionManager.Instance.OnPromotePlayer -= ShowcaseMode;//Nothing to see here ;)
         }
 
         // In case this gambit is triggered externally, trigger every child gambit underneath it
         // (I'm gonna have some fun with Jumper Cables >:)
         public override void Trigger()
         {
+            BaseGambit[] children = FindObjectsByType<BaseGambit>();
             foreach (BaseGambit child in children)
             {
                 if (child is not null)
@@ -61,8 +65,8 @@ namespace Gambonanza.MysterysGambit
 
         // I'm not gonna lie. This was vibecoded. But the idea of this method is to generate only an instance 
         // of a BaseGambit child class without earning a "physical" gambit and adding it to the stock.
-        public BaseGambit CreateGambitInstance(Type childType, GameObject targetObject = null)
-        {
+        public GambitBehaviour CreateGambitInstance(Type childType = null)
+        /*{
             // 1. Ensure the provided type actually inherits from BaseGambit
             if (!typeof(BaseGambit).IsAssignableFrom(childType) || childType == typeof(BaseGambit))
             {
@@ -80,7 +84,7 @@ namespace Gambonanza.MysterysGambit
             // NOTE: I modified the following line to assign Mystery's Gambit's GambitBehaviour
             // component to the child gambit instance. That way every child gambit's fedback
             // animations are anchored to Mystery's Gambit's sprite. 
-            GambitBehaviour behavior = this.m_Gambit;
+            GambitBehaviour behavior = host.GetComponent<GambitBehaviour>();
             if (behavior == null) behavior = host.AddComponent<GambitBehaviour>();
 
             GambitFeedbackIncrementor incrementor = host.GetComponent<GambitFeedbackIncrementor>();
@@ -98,22 +102,71 @@ namespace Gambonanza.MysterysGambit
             feedbackField?.SetValue(newGambit, incrementor);
 
             return newGambit;
+        }*/
+        {
+			SO_Gambit randomUnpossessedGambit = SingletonMonoBehaviour<GambitLibrary>.Instance.GetRandomUnpossessedGambit("MYSTERY");
+            Vector3 zero = new Vector3(x: 0, y:0, z:0);
+			GambitBehaviour gambitBehaviour = Instantiate<GambitBehaviour>(SingletonMonoBehaviour<GambitLibrary>.Instance.Gambits[SingletonMonoBehaviour<GambitLibrary>.Instance.GambitsInfo.IndexOf(randomUnpossessedGambit)], zero, Quaternion.identity);
+			gambitBehaviour.transform.localScale = Vector3.zero;
+
+            // Maybe try this later
+            // BindingFlags flags = BindingFlags.NonPublic | BindingFlags.Instance;
+            // FieldInfo gambitField = typeof(BaseGambit).GetField("m_Gambit", flags);
+            // gambitField.GetValue(gambitBehaviour);
+            return gambitBehaviour;
         }
         // When I make a showcase for this mod, I want to know what child gambits I'm working with
-        private void ShowcaseMode(BasePieceBehaviour piece, TileBehaviour tile)
+        private void ShowcaseMode(PieceType type)
         {
-            if (piece.GetPieceType() == PieceType.PAWN && tile.Position.y < 0)
+            try{
+            if (type == PieceType.PAWN && ChessDataManager.Instance.Coins == 420)
             {
                 for (int x = 0; x < children.Length; x++)
                 {
-                    if (children[x] is not null) Destroy(children[x]);
+                    if (children[x] is not null) Destroy(children[x].gameObject);
                 }
                 children[0] = CreateGambitInstance(typeof(GambitMime));
                 children[1] = CreateGambitInstance(typeof(Violet_Gambit));
                 children[2] = CreateGambitInstance(typeof(MissignoGambit));
                 this.m_FeedbackIncrementor.Spawn(";)");
-			    this.m_FeedbackIncrementor.IncrementSound(0f);
+                this.m_FeedbackIncrementor.IncrementSound(0f);
             }
+            }catch(Exception e)
+            {
+                UpdateDescription(e.ToString());
+            }
+        }
+        //DEBUG
+        public static void UpdateDescription(string s)
+        {
+            var locManager = SingletonMonoBehaviour<LocalizationManager>.Instance;
+            if (locManager == null)
+            {
+                Debug.LogWarning("[GambitApi] LocalizationManager not found, tooltip text will be empty.");
+                return;
+            }
+
+            // Force load if not cached
+            var traduction = locManager.GetTraduction();
+            if (traduction == null)
+            {
+                Debug.LogWarning("[GambitApi] GetTraduction() returned null.");
+                return;
+            }
+
+            var gambitNode = traduction["gambit"];
+            if (gambitNode == null)
+            {
+                Debug.LogWarning("[GambitApi] traduction['gambit'] node not found.");
+                return;
+            }
+
+            gambitNode[$"mystery_description"] = s;
+        }
+
+        private void Handle()
+        {
+
         }
     }
 }
